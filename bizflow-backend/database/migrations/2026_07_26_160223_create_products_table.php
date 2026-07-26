ba318f01c47a23db0adfa0dfbc4503d3738099aa
handikaradyt-ux <handikaradyt@gmail.com>
2026-07-26 23:04:00 +0700

    feat(database): create products migration

---
+++ bizflow-backend/database/migrations/2026_07_26_160223_create_products_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('category_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+
+            $table->string('name', 150);
+
+            $table->string('sku', 50)->unique();
+
+            $table->decimal('price', 12, 2);
+
+            $table->integer('stock')->default(0);
+
+            $table->string('image_path')->nullable();
+
+            $table->enum('status', [
+                'active',
+                'inactive'
+            ])->default('active');
+
+            $table->timestamps();
+
+            $table->softDeletes();
+
+            $table->index('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
