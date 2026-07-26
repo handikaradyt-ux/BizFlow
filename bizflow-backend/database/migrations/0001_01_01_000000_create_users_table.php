14cf7ec1eefd68a2964771234f21c17faad4b72a
handikaradyt-ux <handikaradyt@gmail.com>
2026-07-26 22:58:28 +0700

    feat(database): update users migration

--- bizflow-backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ bizflow-backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+
+            $table->string('name', 100);
+
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+
             $table->string('password');
+
+            $table->enum('role', [
+                'admin',
+                'manager',
+                'cashier'
+            ])->default('cashier');
+
             $table->rememberToken();
+
             $table->timestamps();
         });
 
@@ -42,8 +52,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('users');
     }
 };
